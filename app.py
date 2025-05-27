efbd8e911673be1b83e489e846e39de0a190dc0a
SI-YEONG <siyeong1127@naver.com>
2025-05-28 02:12:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import base64
 
+import matplotlib
 # 폰트 설정 추가
 matplotlib.rc('font', family='Malgun Gothic')
 matplotlib.rcParams['axes.unicode_minus'] = False
